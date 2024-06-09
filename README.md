4127baff9fcd2bb717258e1b16e60bc91b615666
GuilhermeReis12 <guilhermeoliveira12102001@gmail.com>
2024-06-09 20:22:53 -0300

    melhoria no backup

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# backup_banco
-Backup automatico para bancos que estao em servidores ou local 
+# Backup de Banco de Dados SQL Server com Python
 
+Este projeto em Python realiza backups automatizados de um banco de dados SQL Server, salvando os arquivos de backup em um diretório especificado. Ele inclui verificações para garantir que o backup foi concluído com sucesso.
 
-Para rodar de o comando python backup.py
+## Funcionalidades
+
+- Realiza backup completo de um banco de dados SQL Server.
+- Gera nome do arquivo de backup com timestamp.
+- Verifica se o backup foi concluído com sucesso e exibe detalhes do backup.
+- Utiliza variáveis de ambiente para armazenar informações sensíveis.
+
+## Tecnologias Utilizadas
+
+- **Python**: Linguagem de programação utilizada.
+- **pyodbc**: Biblioteca Python para acessar bancos de dados ODBC.
+- **ODBC Driver 17 for SQL Server**: Driver ODBC para conectar ao SQL Server.
+
+## Como Executar
+
+### Pré-requisitos
+
+1. **Python 3.6+**: Certifique-se de ter o Python instalado na sua máquina.
+2. **Pacote pyodbc**: Você pode instalá-lo usando o pip.
+    ```sh
+    pip install pyodbc
+    ```
+3. **Driver ODBC para SQL Server**: Instale o ODBC Driver 17 for SQL Server. [Instruções de instalação](https://docs.microsoft.com/pt-br/sql/connect/odbc/download-odbc-driver-for-sql-server).
+
+### Configuração
+
+Crie um arquivo `.env` na raiz do projeto para definir as variáveis de ambiente necessárias:
+```env
+SQL_SERVER=servidor
+SQL_DATABASE=nome_do_banco
+SQL_USERNAME=nome_de_usuario
+SQL_PASSWORD=senha
+SQL_DRIVER={ODBC Driver 17 for SQL Server}
+
+<!-- pode configurar o drive para o 17 ou 18  -->
--- backup.py
+++ backup.py
@@ -4,100 +4,107 @@ from datetime import datetime
 import time
 
 # Configurações de conexão ao SQL Server
-server = 'servidor'
-database = 'nome do banco'
-username = ' nome de usuario'
-password = 'senha'
-driver = '{ODBC Driver 17 for SQL Server}'
+server = os.getenv('SQL_SERVER', 'servidor')
+database = os.getenv('SQL_DATABASE', 'nome do banco')
+username = os.getenv('SQL_USERNAME', 'nome de usuario')
+password = os.getenv('SQL_PASSWORD', 'senha')
+driver = os.getenv('SQL_DRIVER', '{ODBC Driver 17 for SQL Server}')
 
 # Diretório onde você deseja salvar o backup
 backup_directory = 'C:\\Program Files\\Microsoft SQL Server\\MSSQL15.MSSQLSERVER\\MSSQL\\Backup'
 
-# Nome do arquivo de backup
-backup_filename = f'{database}_backup_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.bak'
+def generate_backup_filename(database):
+    # Gera o nome do arquivo de backup com timestamp
+    return f'{database}_backup_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.bak'
 
-# "DD" representa o dia do mês.
-# "MM" representa o número do mês.
-# "YYYY" representa o ano.
-# "HH" representa a hora.
-# "MM" representa os minutos.
-# "SS" representa os segundos.
+def create_backup_query(database, backup_path):
+    # Cria a consulta SQL para realizar o backup
+    return f"""
+    BACKUP DATABASE {database} 
+    TO DISK = N'{backup_path}' 
+    WITH NOFORMAT, NOINIT, 
+    NAME = N'{database}-Full Database Backup', 
+    SKIP, NOREWIND, NOUNLOAD, STATS = 10
+    """
 
-# Caminho completo do arquivo de backup
-backup_path = os.path.join(backup_directory, backup_filename)
+def check_backup_query():
+    # Consulta SQL para verificar se o backup foi concluído com sucesso
+    return """
+    SELECT
+        A.database_name AS [Database Name],
+        A.backup_start_date AS [Backup Start Date],
+        A.backup_finish_date AS [Backup Finish Date],
+        CASE A.type
+            WHEN 'D' THEN 'Full'
+            WHEN 'I' THEN 'Differential'
+            WHEN 'L' THEN 'Transaction Log'
+            WHEN 'F' THEN 'File/Filegroup'
+            WHEN 'G' THEN 'Differential File'
+            WHEN 'P' THEN 'Partial'
+            WHEN 'Q' THEN 'Differential partial'
+        END AS [Backup Type],
+        B.physical_device_name AS [Backup Location],
+        A.backup_size AS [Backup Size]
+    FROM
+        msdb.dbo.backupset A
+    INNER JOIN msdb.dbo.backupmediafamily B ON A.media_set_id = B.media_set_id
+    WHERE
+        A.database_name = ? 
+        AND A.backup_finish_date = (
+            SELECT
+                MAX(backup_finish_date)
+            FROM
+                msdb.dbo.backupset
+            WHERE
+                database_name = ? 
+                AND type = 'D'
+        );
+    """
 
-# Comando SQL para realizar o backup
-backup_query = f'BACKUP DATABASE {database} TO DISK = N\'{backup_path}\' WITH NOFORMAT, NOINIT, NAME = N\'{database}-Full Database Backup\', SKIP, NOREWIND, NOUNLOAD, STATS = 10'
+def perform_backup():
+    backup_filename = generate_backup_filename(database)
+    backup_path = os.path.join(backup_directory, backup_filename)
+    backup_query = create_backup_query(database, backup_path)
 
-# Consulta SQL para verificar se o backup foi concluído com sucesso
-check_backup_query = """
-SELECT
-    A.database_name AS [Database Name],
-    A.backup_start_date AS [Backup Start Date],
-    A.backup_finish_date AS [Backup Finish Date],
-    CASE A.type
-        WHEN 'D' THEN 'Full'
-        WHEN 'I' THEN 'Differential'
-        WHEN 'L' THEN 'Transaction Log'
-        WHEN 'F' THEN 'File/Filegroup'
-        WHEN 'G' THEN 'Differential File'
-        WHEN 'P' THEN 'Partial'
-        WHEN 'Q' THEN 'Differential partial'
-    END AS [Backup Type],
-    B.physical_device_name AS [Backup Location],
-    A.backup_size AS [Backup Size]
-FROM
-    msdb.dbo.backupset A
-INNER JOIN msdb.dbo.backupmediafamily B ON A.media_set_id = B.media_set_id
-WHERE
-    A.database_name = ? -- Filtra pelo nome do banco de dados
-    AND A.backup_finish_date = (
-        SELECT
-            MAX(backup_finish_date)
-        FROM
-            msdb.dbo.backupset
-        WHERE
-            database_name = ? -- Filtra pelo nome do banco de dados
-            AND type = 'D' -- Assumindo que você está interessado em backups completos, ajuste conforme necessário
-    );
-"""
+    try:
+        # Conecta ao SQL Server
+        conn = pyodbc.connect(
+            f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+        conn.autocommit = True
+        cursor = conn.cursor()
 
-try:
-    # Conecta ao SQL Server
-    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+        # Executa o comando de backup
+        cursor.execute(backup_query)
+        print(f'Backup do banco de dados {database} iniciado. Caminho do backup: {backup_path}')
 
-    # Ativa o autocommit
-    conn.autocommit = True
+        # Verifica se o backup foi concluído com sucesso
+        while True:
+            cursor.execute(check_backup_query(), (database, database))
+            row = cursor.fetchone()
+            if row:
+                print('Backup concluído com sucesso!')
+                print('Detalhes do Backup:')
+                print(f'Nome do Banco de Dados: {row[0]}')
+                print(f'Data e Hora de Início do Backup: {row[1]}')
+                print(f'Data e Hora de Conclusão do Backup: {row[2]}')
+                print(f'Tipo de Backup: {row[3]}')
+                print(f'Localização do Backup: {row[4]}')
+                print(f'Tamanho do Backup: {row[5]}')
+                break
+            else:
+                print('Aguardando conclusão do backup...')
+                time.sleep(10)  # Verifica a cada 10 segundos se o backup foi concluído
 
-    # Cria um cursor
-    cursor = conn.cursor()
+    except pyodbc.Error as e:
+        print(f'Erro ao realizar o backup: {e}')
 
-    # Executa o comando de backup
-    cursor.execute(backup_query)
+    except Exception as e:
+        print(f'Erro inesperado: {e}')
 
-    print(f'Backup do banco de dados {database} iniciado. Caminho do backup: {backup_path}')
+    finally:
+        # Fecha a conexão
+        if 'conn' in locals() and conn is not None:
+            conn.close()
 
-    # Verifica se o backup foi concluído com sucesso
-    while True:
-        cursor.execute(check_backup_query, (database, database))
-        row = cursor.fetchone()
-        if row:
-            print('Backup concluído com sucesso!')
-            print('Detalhes do Backup:')
-            print(f'Nome do Banco de Dados: {row[0]}')
-            print(f'Data e Hora de Início do Backup: {row[1]}')
-            print(f'Data e Hora de Conclusão do Backup: {row[2]}')
-            print(f'Tipo de Backup: {row[3]}')
-            print(f'Localização do Backup: {row[4]}')
-            print(f'Tamanho do Backup: {row[5]}')
-            break
-        else:
-            print('Aguardando conclusão do backup...')
-            time.sleep(10)  # Verifica a cada 10 segundos se o backup foi concluído
-
-except pyodbc.Error as e:
-    print(f'Erro ao realizar o backup: {e}')
-
-finally:
-    # Fecha a conexão
-    conn.close()
+if __name__ == '__main__':
+    perform_backup()
